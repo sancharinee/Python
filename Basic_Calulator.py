641edfbcb82ec1034eb16b397d93fb86d78960af
sancharinidey <67041010+sancharinidey@users.noreply.github.com>
2021-06-21 11:24:37 +0530

    Add files via upload

---
+++ Basic_Calulator.py
@@ -0,0 +1,90 @@
+Python 3.8.3 (tags/v3.8.3:6f8c832, May 13 2020, 22:20:19) [MSC v.1925 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+= RESTART: C:/Users/SONU/AppData/Local/Programs/Python/Python38-32/Calculator2.py
+Calculator
+Following functionality is provided in calculator
+a) Basic calculator
+b) Scientific calculator
+c) Exit calculator
+Enter your choice : a
+
+Basic calculator
+Following functionality is provided in basic calculator
+a) Addition
+b) Substraction
+c) Multiplication
+d) Division
+e) Exit basic calculator
+Enter your choice : d
+
+Division
+Enter num1 = 23
+Enter num2 = 12
+23 / 12 = 1.9166666666666667
+
+Continue division (y/n) : y
+
+Division
+Enter num1 = 23
+Enter num2 = 33
+23 / 33 = 0.696969696969697
+
+Continue division (y/n) : n
+
+Basic calculator
+Following functionality is provided in basic calculator
+a) Addition
+b) Substraction
+c) Multiplication
+d) Division
+e) Exit basic calculator
+Enter your choice : e
+Exited from basic calculator
+
+Calculator
+Following functionality is provided in calculator
+a) Basic calculator
+b) Scientific calculator
+c) Exit calculator
+Enter your choice : b
+
+Scientific calculator
+Following functionality is provided in scientific calculator
+a) Trignometric ratios
+b) y to the power x
+c) Logarithm
+d) Exit scientific calculator
+Enter your choice : c
+
+Logarihtm
+Enter base = 21
+Enter argument = 12
+log12 base21 = 0.8161893041084362
+
+Continue logarithm (y/n) : 
+
+Logarihtm
+Enter base = 23
+Enter argument = 12
+log12 base23 = 0.7925087653372185
+
+Continue logarithm (y/n) : n
+
+Scientific calculator
+Following functionality is provided in scientific calculator
+a) Trignometric ratios
+b) y to the power x
+c) Logarithm
+d) Exit scientific calculator
+Enter your choice : d
+Exited from scientific calculator
+
+Calculator
+Following functionality is provided in calculator
+a) Basic calculator
+b) Scientific calculator
+c) Exit calculator
+Enter your choice : c
+Exited from calculator
+>>> 
